1f590007391e6afca24c435f3284591b0a4e832d
alexanderNer <nerheimalexander@gmail.com>
2025-02-11 12:49:08 +0100

    fortsettelse

--- indeks.js
+++ indeks.js
@@ -5,16 +5,33 @@ let scoreTxt = container.querySelector(".score");
 let guideTxt = container.querySelector(".guide-Txt");
 
 let gamePlay = false;
+let birdBottom = 300;
+let birdLeft = 20;
+let gravity = 3;
+let startId;
 
 document.addEventListener("keydown", (e) => {
-    if (e.code == "Space"){
-        if(!gamePlay){
+    if (e.code == "Space") {
+        if (!gamePlay) {
+
+            startGame();
+
             scoreTxt.style.display = "block";
             gameIcon.style.display = "none";
             guideTxt.style.display = "none";
         }
-
+        gamePlay = true;
     }
-
-        
 })
+
+let startGame = () =>{
+    birdDown();
+    startId = setInterval(birdDown, 20); 
+}
+
+let birdDown = () =>{
+    birdBottom -= gravity;
+    bird.style.Bottom = birdBottom + "px"; 
+    bird.style.left = birdLeft + "px";
+
+}
